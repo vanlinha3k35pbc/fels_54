e148e816d8a689b90e0c70d04879a68418eec4f9
ngocvnn <nguyen.thi.ngocb@framgia.com>
2015-04-23 14:29:27 +0700

    admin-category

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,48 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.paginate page: params[:page], per_page: 15
+  end
+
+  def show
+    @category = Category.find params[:id]
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit
+    @category = Category.find params[:id]
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      redirect_to [:admin, @category]
+    else
+      render :new
+    end
+  end
+
+  def update
+    @category = Category.find params[:id]
+    if @category.update_attributes category_params
+      flash[:success] = "Category updated"
+      redirect_to [:admin, @category]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Category.find(params[:id]).destroy
+    flash[:success] = "Category deleted"
+    redirect_to admin_categories_url  
+  end
+
+  private 
+  def category_params
+    params.require(:category).permit(:title, :description)
+  end
+
+end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,5 @@
 class Word < ActiveRecord::Base
+  belongs_to :category
+
+  has_many :answers
 end
